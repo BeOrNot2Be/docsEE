0e02e52b8949f083a142c09260ce33b3a17c361e
BeOrNot2Be <timchick.ua@gmail.com>
2019-11-21 23:18:52 -0800

    testing 1.1/ JPA unit tests

--- src/main/java/beornot2be/docsEE/model/Document.java
+++ src/main/java/beornot2be/docsEE/model/Document.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.Set;
 import javax.persistence.*;
 
+
 @Entity
 @Table(name = "Document")
 public class Document implements Serializable{
---
+++ src/main/java/beornot2be/docsEE/model/DocumentFileRepository.java
@@ -0,0 +1,12 @@
+package beornot2be.docsEE.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface DocumentFileRepository extends JpaRepository<DocumentFile, Integer>{
+    public Optional<DocumentFile> findById(Integer id);
+
+}
